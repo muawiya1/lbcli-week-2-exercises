2b8604324fde3b1f3ffa91e00cbbcdc846fc64af
muawiya1 <muawiyaabk@gmail.com>
2026-03-25 14:07:07 +0100

    Fix 03

--- submission/03.sh
+++ submission/03.sh
@@ -2,6 +2,6 @@
 # Add funds to the address.
 # Return only the Address
 
-ADDR_Segwit= $(bitcoin-cli -regtest getnewaddress p2sh-segwit)
-bitcoin-cli -regtest sendtoaddress $ADDR_Segwit 0.1
-echo $ADDR_Segwit
+ADDR_Segwit=$(bitcoin-cli -regtest getnewaddress "" "bech32")
+bitcoin-cli -regtest generatetoaddress 101 "$ADDR_Segwit" > /dev/null 2>&1
+echo "$ADDR_Segwit"
