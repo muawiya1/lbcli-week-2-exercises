23a823b0c6d834533b08d081688ba6bb25ccf358
muawiya1 <muawiyaabk@gmail.com>
2026-03-25 13:47:56 +0100

    Fix 01

--- submission/03.sh
+++ submission/03.sh
@@ -1,3 +1,7 @@
 # Create a SegWit address.
 # Add funds to the address.
 # Return only the Address
+
+ADDR_Segwit= $(bitcoin-cli -regtest getnewaddress p2sh-segwit)
+bitcoin-cli -regtest sendtoaddress $ADDR_Segwit 0.1
+echo $ADDR_Segwit
